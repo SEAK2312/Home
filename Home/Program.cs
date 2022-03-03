6bda2ce94db350bb00378b1ddc8fca39b47457a9
Miron Melnikov <melnikovmiron33@gmail.com>
2022-03-03 20:38:57 +0300

    Домашнее задание 1.3

--- Home/Program.cs
+++ Home/Program.cs
@@ -5,73 +5,124 @@ using System.Text;
 
 namespace Console_RPG
 {
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            while (true)
+
+            Console.Write("Введите имя: ");
+            string playerName = Console.ReadLine();
+            string type = "", bonus = "";
+            int choise, hp = 100, dmg = 30, luck = 30;
+            Random rnd = new Random();
+            int ih = rnd.Next(1, 10);
+            for (int i = 0; i < 10; i++)
             {
-                Console.Write("Введите имя: ");
-                string playerName = Console.ReadLine();
-                Random r = new Random();
-                for (int km = r.Next(1, 10) + 1; km <= r.Next(10, 10) + 1; km++)
+
+                while (type == "")
                 {
-                    for (int i = 0; i < 10; i++)
+                    Console.WriteLine("--(1 - маг огня, 2 -маг воды, 3 -маг земли)--");
+                    Console.Write("Выберите тип персонажа: ");
+                    choise = int.Parse(Console.ReadLine());
+                    switch (choise)
                     {
-                        while (true)
-                        {
-                           
-                                Console.WriteLine("--(1 - идти, 2 - говорить, 3 - покушать, 4 - отдохнуть, 5 - пойти домой)--");
-                                Console.Write("Выберите действие: ");
-                                int numOfAction = int.Parse(Console.ReadLine());
-                                if (numOfAction == 1)
-                                {
-                                    Console.WriteLine($"{playerName} Он пошел в домик на дереве");
-                                    Console.WriteLine($"{playerName} Он прошел {km} км");
-                                    int hours = r.Next(1, 6);
-                                    Console.WriteLine($"{playerName} Он шел {hours} часов");
-                                }
-                                else if (numOfAction == 2)
-                                {
-                                    int hours = r.Next(1, 6);
-                                    Console.WriteLine($"{playerName} Он разговаривал с подругой");
-                                    Console.WriteLine($"{playerName} Он говорил с подругой {hours} часов");
-                                }
-                                else if (numOfAction == 3)
-                                {
-                                    int hours = r.Next(1, 6);
-                                    Console.WriteLine($"{playerName} Он пошел поесть с подругой");
-                                    Console.WriteLine($"{playerName} Он кушал с подругой {hours} часов");
-                                }
-                                else if (numOfAction == 4)
-                                {
-                                    Console.WriteLine($"{playerName} Он пошел полежать с подругой");
-                                    int hours = r.Next(1, 6);
-                                    Console.WriteLine($"{playerName} Он спал с подругой {hours} часов");
-                                }
-                                else if (numOfAction == 5)
-                                {
-                                    Console.WriteLine($"{playerName} Он пошел к себе домой");
-                                    Console.WriteLine($"{playerName} Он прошел {km} км");
-                                    int hours = r.Next(1, 6);
-                                    Console.WriteLine($"{playerName} Он шел обратно к себе домой {hours} часов");
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"{playerName} Он не умеет выполнять эту задачу");
-                                }
-                        } 
-                    }
-                    Console.WriteLine("Для продолжения нажмите Enter, а для выхода нажмите 0: ");
-                    if (Console.ReadLine() == "0")
-                    {
-                        break;
+                        case 1:
+                            type = "маг огня";
+                            break;
+                        case 2:
+                            type = "маг воды";
+                            break;
+                        case 3:
+                            type = "маг земли";
+                            break;
+                        default:
+                            Console.WriteLine("Такого типа не существует");
+                            break;
                     }
                 }
-                Console.ReadKey();
+                Console.WriteLine($"Вы выбрали тип персонажа: {type}");
+
+
+                while (bonus == "")
+                {
+                    Console.WriteLine("--(1 - больше маны, 2 - больше урона, 3 - больше защиты)--");
+                    Console.Write("Выберите бонус персонажа: ");
+                    choise = int.Parse(Console.ReadLine());
+
+                    switch (choise)
+                    {
+
+
+                        case 1:
+                            bonus = "больше маны";
+                            i = 3;
+                            break;
+                        case 2:
+                            bonus = "больше урона";
+                            i = 6;
+                            break;
+                        case 3:
+                            bonus = " больше защиты";
+                            i = 10;
+                            break;
+                        default:
+                            Console.WriteLine("Такого типа не существует");
+                            break;
+
+                    }
+                    Console.WriteLine($"Вы выбрали бонус: {bonus}");
+                }
             }
-        }
-    }
-}
+            Random r = new Random();
+            for (int km = 1; km <= 7; km++)
+            {
+                for (int it = 0; it < 10; it++)
+                {
+                    Console.WriteLine("--(1 - замок, 2 - говорить, 3 - задание, 4 - троффей,5 - пойти спать)--");
+                    Console.Write("Выберите действие: ");
+                    int numOfAction = int.Parse(Console.ReadLine());
+                    switch (numOfAction)
+                    {
+                        case 1:
+                            Console.WriteLine($"{playerName} пошел на обучение в волшебный замок");
+                            int hoursing = r.Next(1, 8);
+                            Console.WriteLine($"{playerName} Пришел в замок за {hoursing} км");
+                            it = 1;
+                            break;
+                        case 2:
+                            Console.WriteLine($"{playerName} разговаривал с мудрецом волшебства");
+                            it = 3;
+                            break;
+                        case 3:
+                            int hours = r.Next(1, 8);
+                            Console.WriteLine($"{playerName} пошел сразить монстра за {hours} часов");
+                            it = 5;
+                            break;
+                        case 4:
+                            Console.WriteLine($"{playerName} принес голову монстра мудрецу");
+                            it = 7;
+                            break;
+                        case 5:
+                            Console.WriteLine($"{playerName} Пошел в замок что бы поспать");
+                            it = 10;
+                            break;
+                        default:
+                            Console.WriteLine($"{playerName} Он не умеет выполнять эту задачу");
+                            break;
+                    }
+
+                }
+            
+              Console.WriteLine($"{playerName} Он закончил свой {km} день");
+              Console.WriteLine("Для продолжения нажмите Enter, а для выхода нажмите 0: ");
+              if (Console.ReadLine() == "0")
+              {
+                break;
+              }
+              Console.ReadKey();
+            }
+        }           
+    } 
+} 
+
